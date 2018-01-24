087c33599f0b090eb323fe6e239361f31a099c37
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-01-23 17:16:57 -0800

    adds the checkoff markup

--- src/app/components/CheckOff/CheckOffPage.js
+++ src/app/components/CheckOff/CheckOffPage.js
@@ -5,12 +5,37 @@ require('../../scss/style.scss');
 
 class CheckOffPage extends React.Component {
     constructor(props) {
-        super();
+        super(props);
     }
 
     render() {
         return(
-            <h1>Check Off</h1>
+            <section className="checkoff">
+                <h1>Checkoff</h1>
+                <div className="checkoff-menu-bar">
+                    <div className="container-full">
+                        <div className="part-info">
+                            <div className="part-leader-avatar"><img alt="part-leader-profile-pic"/></div>
+                            {/* <div className="part-leader-name">{{ partLeaderName }}</div> */}
+                            {/* <div className="part-leader-part">{{ partLeaderPart }}</div> */}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="part-members">
+                    <div className="container-full">
+                        <div className="col-lg-3 col-md-6 col-sm-12">
+                            <div className="member" onClick={this.seeMemberSongs.bind(this)}>
+                                <img src="" alt="user-avatar"/>
+                                {/* <h4>{{member.firstName}} {{ member.lastName }}</h4> */}
+                                {/* <span className="member-percentage">{{member.percentage}}%</span> */}
+                                {/* <span className="song-completed">{{member.completedSongs}} / {{ member.songs.length }}</span> */}
+                                {/* <span className="start-date">{{member.startDate}}</span> */}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         )
     }
 }
