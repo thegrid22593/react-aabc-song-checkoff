d5ca8f8f6e035142e545e6347597ac02c1dd665e
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-01-25 13:57:15 -0800

    adds prettier, formats a few files, organizes the sidebar into common for its styles

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, createStore } from 'redux';
-import {createLogger} from "redux-logger";
-import thunk from "redux-thunk";
-import promise from "redux-promise-middleware";
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
@@ -14,7 +14,7 @@ const middleware = applyMiddleware(promise(), thunk, createLogger());
 const store = createStore(reducer, stateLoader.loadState(), middleware);
 
 store.subscribe(() => {
-    stateLoader.saveState(store.getState());
+  stateLoader.saveState(store.getState());
 });
 
 module.exports = store;
