4191bc9992f641a4254179560065bbd2313e795d
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-01-29 17:47:55 -0800

    more styling tweaks

--- src/app/components/UserSettings/UserSettings.js
+++ src/app/components/UserSettings/UserSettings.js
@@ -42,86 +42,88 @@ class UserSettings extends React.Component {
    render() {
       return (
          <div className="settings">
-            <h1>Public Profile</h1>
-            {/* <h1>Hello {{currentUserName}}</h1> */}
-            <form>
-               <div className="user-pic-container">
-                  <img className="profile-pic" src="" alt="" />
-                  <span>
-                     <i className="fa fa-camera" aria-hidden="true" />Upload
-                     Photo
-                  </span>
-               </div>
+            <div className="settings-content">
+               <h1>Public Profile</h1>
+               {/* <h1>Hello {{currentUserName}}</h1> */}
+               <form>
+                  <div className="user-pic-container">
+                     <img className="profile-pic" src="" alt="" />
+                     <span>
+                        <i className="fa fa-camera" aria-hidden="true" />Upload
+                        Photo
+                     </span>
+                  </div>
 
-               <input type="text" name="name" />
-               {/* <input type="file" id="filebutton"/> */}
-               {/* <div>{{uploadProgress}}</div> */}
-               <button type="button">Update</button>
-            </form>
+                  <input type="text" name="name" />
+                  {/* <input type="file" id="filebutton"/> */}
+                  {/* <div>{{uploadProgress}}</div> */}
+                  <button type="button">Update</button>
+               </form>
 
-            <form action="" className="user-settings">
-               <div>
-                  <label for="firstName">First Name</label>
-                  <input
-                     type="text"
-                     onChange={this.handleChange.bind(this)}
-                     name="firstName"
-                     value={this.state.firstName}
-                  />
-               </div>
-               <div>
-                  <label for="lastName">Last Name</label>
-                  <input
-                     type="text"
-                     onChange={this.handleChange.bind(this)}
-                     name="lastName"
-                     value={this.state.lastName}
-                  />
-               </div>
-               <div>
-                  <label for="startDate">Start Date</label>
-                  <input
-                     type="text"
-                     onChange={this.handleChange.bind(this)}
-                     name="startDate"
-                     value={this.state.startDate}
-                  />
-               </div>
-               <div>
-                  <label for="singingPart">Part</label>
-                  <input
-                     type="text"
-                     onChange={this.handleChange.bind(this)}
-                     name="singingPart"
-                     value={this.state.singingPart}
-                  />
-               </div>
-               <div>
-                  <label for="partLeader">Partleader?</label>
-                  <input
-                     type="radio"
-                     onChange={this.handleChange.bind(this)}
-                     name="partLeader"
-                     value="true"
-                  />{' '}
-                  Yes <br />
-                  <input
-                     type="radio"
-                     onChange={this.handleChange.bind(this)}
-                     name="partLeader"
-                     value="false"
-                  />{' '}
-                  No
-               </div>
-               <div>
-                  <button
-                     onClick={this.updateSettings.bind(this)}
-                     type="button"
-                  >
-                     Save Settings
-                  </button>
-               </div>
-            </form>
+               <form action="" className="user-settings">
+                  <div>
+                     <label for="firstName">First Name</label>
+                     <input
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        name="firstName"
+                        value={this.state.firstName}
+                     />
+                  </div>
+                  <div>
+                     <label for="lastName">Last Name</label>
+                     <input
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        name="lastName"
+                        value={this.state.lastName}
+                     />
+                  </div>
+                  <div>
+                     <label for="startDate">Start Date</label>
+                     <input
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        name="startDate"
+                        value={this.state.startDate}
+                     />
+                  </div>
+                  <div>
+                     <label for="singingPart">Part</label>
+                     <input
+                        type="text"
+                        onChange={this.handleChange.bind(this)}
+                        name="singingPart"
+                        value={this.state.singingPart}
+                     />
+                  </div>
+                  <div>
+                     <label for="partLeader">Partleader?</label>
+                     <input
+                        type="radio"
+                        onChange={this.handleChange.bind(this)}
+                        name="partLeader"
+                        value="true"
+                     />{' '}
+                     Yes <br />
+                     <input
+                        type="radio"
+                        onChange={this.handleChange.bind(this)}
+                        name="partLeader"
+                        value="false"
+                     />{' '}
+                     No
+                  </div>
+                  <div>
+                     <button
+                        onClick={this.updateSettings.bind(this)}
+                        type="button"
+                     >
+                        Save Settings
+                     </button>
+                  </div>
+               </form>
+            </div>
          </div>
       );
    }
