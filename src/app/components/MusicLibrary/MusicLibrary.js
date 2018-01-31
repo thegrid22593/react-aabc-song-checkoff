09b7ac74701fe0bb87c5d95ca947cf56fbfb1d09
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-01-31 11:09:23 -0800

    starts the part comparison

--- src/app/components/MusicLibrary/MusicLibrary.js
+++ src/app/components/MusicLibrary/MusicLibrary.js
@@ -4,106 +4,110 @@ import MusicLibrarySongDetail from './MusicLibrarySongDetail';
 import MusicLibrarySearch from './MusicLibrarySearch';
 
 class MusicLibrary extends React.Component {
-    constructor(props) {
-        super(props);
+   constructor(props) {
+      super(props);
 
-        this.state = {
+      this.state = {
+         songDetailIsActive: false,
+         activeSong: {
+            parts: [],
+         },
+         songs: this.props.songs,
+      };
+   }
+
+   componentWillReceiveProps(nextProps) {
+      this.setState({ songs: nextProps.songs });
+   }
+
+   toggleSongDetail(e, index) {
+      e.preventDefault();
+      console.log('open detail', e);
+      console.log('index', index);
+      console.log('this', this);
+      if (index !== null) {
+         let activeSong = {
+            index: index,
+            ...this.props.songs[index],
+         };
+         this.setState({
+            activeSong: activeSong,
+            songDetailIsActive: true,
+         });
+      } else {
+         this.setState({
             songDetailIsActive: false,
-            activeSong: {
-                parts: [],
-            },
-            songs: this.props.songs,
-        };
-    }
+         });
+      }
+   }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ songs: nextProps.songs });
-    }
+   completedSong() {
+      console.log('fired');
+      let index = this.state.activeSong.index;
+      this.props.songs[index].completed = true;
+      this.props.updateUserSongs(this.props.songs[index]);
+   }
 
-    toggleSongDetail(e, index) {
-        e.preventDefault();
-        console.log('open detail', e);
-        console.log('index', index);
-        console.log('this', this);
-        if (index !== null) {
-            let activeSong = {
-                index: index,
-                ...this.props.songs[index],
-            };
-            this.setState({
-                activeSong: activeSong,
-                songDetailIsActive: true,
-            });
-        } else {
-            this.setState({
-                songDetailIsActive: false,
-            });
-        }
-    }
+   searchLibrary(e) {
+      console.log('working', e.target.value);
+      let searchValue = e.target.value.toLowerCase();
+      let searchedSongs = this.props.songs.filter(song => {
+         return song.name.toLowerCase().includes(searchValue);
+      });
+      this.setState({ songs: searchedSongs });
+   }
 
-    completedSong() {
-        console.log('fired');
-        let index = this.state.activeSong.index;
-        this.props.songs[index].completed = true;
-        this.props.updateUserSongs(this.props.songs[index]);
-    }
+   handleSelectChange(e) {
+      console.log('working', e.target.value);
+      let sortValue = e.target.value;
+      let songsToSort = this.props.songs;
+      let sortedSongs;
+      if (sortValue === 'difficulty-ascending') {
+         sortedSongs = songsToSort.sort((a, b) => {
+            let x = a.difficulty;
+            let y = b.difficulty;
+            return x < y ? -1 : x > y ? 1 : 0;
+         });
+      } else if (sortValue === 'difficulty-descending') {
+         sortedSongs = songsToSort.sort((a, b) => {
+            let x = a.difficulty;
+            let y = b.difficulty;
+            return x > y ? -1 : x < y ? 1 : 0;
+         });
+      } else if (sortValue === 'time-ascending') {
+         sortedSongs = songsToSort.sort((a, b) => {
+            let x = parseFloat(a.time.replace(':', ''));
+            let y = parseFloat(b.time.replace(':', ''));
+            return x > y ? -1 : x < y ? 1 : 0;
+         });
+      } else if (sortValue === 'time-descending') {
+         sortedSongs = songsToSort.sort((a, b) => {
+            let x = parseFloat(a.time.replace(':', ''));
+            let y = parseFloat(b.time.replace(':', ''));
+            return x < y ? -1 : x > y ? 1 : 0;
+         });
+         // We need to get a new set of songs with this
+      } else if (sortValue === 'completed') {
+         sortedSongs = songsToSort.filter(song => {
+            return song.completed === true;
+         });
+      } else if (sortValue === 'not-completed') {
+         sortedSongs = songsToSort.filter(song => {
+            return song.completed === false;
+         });
+      }
+      this.setState({ songs: sortedSongs });
+   }
 
-    searchLibrary(e) {
-        console.log('working', e.target.value);
-        let searchValue = e.target.value.toLowerCase();
-        let searchedSongs = this.props.songs.filter(song => {
-            return song.name.toLowerCase().includes(searchValue);
-        });
-        this.setState({ songs: searchedSongs });
-    }
-
-    handleSelectChange(e) {
-        console.log('working', e.target.value);
-        let sortValue = e.target.value;
-        let songsToSort = this.state.songs;
-        let sortedSongs;
-        if (sortValue === 'difficulty-ascending') {
-            sortedSongs = songsToSort.sort((a, b) => {
-                let x = a.difficulty;
-                let y = b.difficulty;
-                return x < y ? -1 : x > y ? 1 : 0;
-            });
-        } else if (sortValue === 'difficulty-descending') {
-            sortedSongs = songsToSort.sort((a, b) => {
-                let x = a.difficulty;
-                let y = b.difficulty;
-                return x > y ? -1 : x < y ? 1 : 0;
-            });
-        } else if (sortValue === 'time-ascending') {
-            sortedSongs = songsToSort.sort((a, b) => {
-                let x = parseFloat(a.time.replace(':', ''));
-                let y = parseFloat(b.time.replace(':', ''));
-                return x > y ? -1 : x < y ? 1 : 0;
-            });
-        } else if (sortValue === 'time-descending') {
-            sortedSongs = songsToSort.sort((a, b) => {
-                let x = parseFloat(a.time.replace(':', ''));
-                let y = parseFloat(b.time.replace(':', ''));
-                return x < y ? -1 : x > y ? 1 : 0;
-            });
-            // We need to get a new set of songs with this
-        } else if (sortValue === 'completed') {
-            sortedSongs = songsToSort.filter((song) => { return song.completed === true; })
-        } else if (sortValue === 'not-completed') {
-            sortedSongs = songsToSort.filter((song) => { return song.completed === false })
-        }
-        this.setState({ songs: sortedSongs });
-    }
-
-    render() {
-        return (
-            <div className="music-library">
-                <MusicLibrarySearch
-                    searchLibrary={e => this.searchLibrary(e)}
-                    handleSelectChange={e => this.handleSelectChange(e)}
-                    songs={this.state.songs}
-                />
-                {/* <div className="col-lg-2">
+   render() {
+      return (
+         <div className="music-library">
+            <MusicLibrarySearch
+               searchLibrary={e => this.searchLibrary(e)}
+               handleSelectChange={e => this.handleSelectChange(e)}
+               songs={this.state.songs}
+            />
+            {/* <div className="col-lg-2">
                <section className="song-filter">
                   <h4>Filter</h4>
                   <hr className="filter-hr" />
@@ -131,51 +135,51 @@ class MusicLibrary extends React.Component {
                   </ul>
                </section>
             </div> */}
-                <section className="songs">
-                    {/* <div className="spinner">
+            <section className="songs">
+               {/* <div className="spinner">
                             <div className="bounce1"></div>
                             <div className="bounce2"></div>
                             <div className="bounce3"></div>
                             <div className="bounce4"></div>
                         </div> */}
-                    <div className="songs-list">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Difficulty</th>
-                                    <th>Time</th>
-                                </tr>
-                            </thead>
+               <div className="songs-list">
+                  <table className="table table-hover">
+                     <thead>
+                        <tr>
+                           <th>Name</th>
+                           <th>Difficulty</th>
+                           <th>Time</th>
+                        </tr>
+                     </thead>
 
-                            {this.state.songs.map((song, index) => {
-                                return (
-                                    <tbody className={song.completed ? 'completed' : ''}>
-                                        <tr
-                                            key={index}
-                                            onClick={e => {
-                                                this.toggleSongDetail(e, index);
-                                            }}
-                                        >
-                                            <td>{song.name}</td>
-                                            <td>{song.difficulty}</td>
-                                            <td>{song.time}</td>
-                                        </tr>
-                                    </tbody>
-                                );
-                            })}
-                        </table>
-                    </div>
-                </section>
-                <MusicLibrarySongDetail
-                    isActive={this.state.songDetailIsActive}
-                    toggleSongDetail={this.toggleSongDetail.bind(this)}
-                    activeSong={this.state.activeSong}
-                    completedSong={this.completedSong.bind(this)}
-                />
-            </div>
-        );
-    }
+                     {this.state.songs.map((song, index) => {
+                        return (
+                           <tbody className={song.completed ? 'completed' : ''}>
+                              <tr
+                                 key={index}
+                                 onClick={e => {
+                                    this.toggleSongDetail(e, index);
+                                 }}
+                              >
+                                 <td>{song.name}</td>
+                                 <td>{song.difficulty}</td>
+                                 <td>{song.time}</td>
+                              </tr>
+                           </tbody>
+                        );
+                     })}
+                  </table>
+               </div>
+            </section>
+            <MusicLibrarySongDetail
+               isActive={this.state.songDetailIsActive}
+               toggleSongDetail={this.toggleSongDetail.bind(this)}
+               activeSong={this.state.activeSong}
+               completedSong={this.completedSong.bind(this)}
+            />
+         </div>
+      );
+   }
 }
 
 module.exports = MusicLibrary;
--- src/app/components/UserDashboard/UserDashboardPartComparison.js
+++ src/app/components/UserDashboard/UserDashboardPartComparison.js
@@ -1,18 +1,69 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 require('../../scss/style.scss');
 
 class UserDashboardPartComparison extends React.Component {
-   constructor() {
-      super();
+   constructor(props) {
+      super(props);
    }
 
    componentWillMount() {}
 
    render() {
-      return <h1>Part Comparison</h1>;
+      return (
+         <section className="part-comparison">
+            <div className="part-graphs">
+               <div className="part">
+                  <div className="part-name">First Tenor</div>
+                  <div className="part-percentage-bar">
+                     <div
+                        className="part-percentage"
+                        style={{ backgroundColor: '#469ff3' }}
+                     />
+                  </div>
+               </div>
+               <div className="part">
+                  <div className="part-name">Second Tenor</div>
+                  <div className="part-percentage-bar">
+                     <div
+                        className="part-percentage"
+                        style={{ backgroundColor: '#2ECC71' }}
+                     />
+                  </div>
+               </div>
+               <div className="part">
+                  <div className="part-name">Baritone</div>
+                  <div className="part-percentage-bar">
+                     <div
+                        className="part-percentage"
+                        style={{ backgroundColor: '#f3cc46' }}
+                     />
+                  </div>
+               </div>
+               <div className="part">
+                  <div className="part-name">Bass</div>
+                  <div className="part-percentage-bar">
+                     <div
+                        className="part-percentage"
+                        style={{ backgroundColor: '#A9171C' }}
+                     />
+                  </div>
+               </div>
+            </div>
+         </section>
+      );
    }
 }
+
+UserDashboardPartComparison = withRouter(
+   connect(store => {
+      return {
+         user: store.user.user,
+      };
+   })(UserDashboardPartComparison)
+);
 
 module.exports = UserDashboardPartComparison;
