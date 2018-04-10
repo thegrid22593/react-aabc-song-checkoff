91b40178f8dc332ebd574ce71f6222d011950ef1
Garrett Sanderson <garrett.sanderson@gmail.com>
2018-04-09 17:39:26 -0700

    user settings form

---
+++ src/app/components/UserSettings/UserSettingsForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserSettingsForm extends Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      return false;
+   }
+}
+
+export default UserSettingsForm;
